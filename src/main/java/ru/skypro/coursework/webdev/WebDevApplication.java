908e1e96df297680ed067b753c2f89d8074a9c66
ya121 <ya7771@yandex.ru>
2023-07-01 17:20:46 +0300

    Начальный коммит к Курсовой работе Web Development

---
+++ src/main/java/ru/skypro/coursework/webdev/WebDevApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.coursework.webdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDevApplication.class, args);
+    }
+
+}
